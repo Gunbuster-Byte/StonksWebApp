cbdb998298d3dea096979998bdd04ab084c6837a
Jason Rodriguez <austinwithaustism@Jasons-iMac.local>
2020-07-05 19:24:33 -0400

    data

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+const mongoose = require('mongoose');
 
+// const cors = require('cors');
+const router = require('./routes/index');
+
+const PORT = process.env.PORT || 5000; 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://heroku_81whdc6t:jhGPEHVjjyzpeAlR1@ds033599.mlab.com:33599/heroku_81whdc6t";
+const uri = "mongodb+srv://Jason_Rod:pEyQefCge8UuYH0l@cop4331-7exdq.mongodb.net/COP4331?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 client.connect(err => {
@@ -16,6 +21,18 @@ client.connect(err => {
   client.close();
 });
 
+// app.use(cors())
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use('/api', router); 
+
+mongoose.connect(uri, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connection.once('open', function() { 
+  console.log('Connected to the Database.');
+});
+mongoose.connection.on('error', function(error) {
+  console.log('Mongoose Connection Error : ' + error);
+});
 
 app.use((req, res, next) => 
 {  
@@ -30,4 +47,6 @@ app.use((req, res, next) =>
     );  
     next();
 });
-app.listen(process.env.PORT || 5000); // start Node + Express server on port 5000
+app.listen(PORT, function() { 
+  console.log(`Server listening on port ${PORT}.`);
+});
