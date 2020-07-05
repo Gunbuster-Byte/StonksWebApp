89d95b66971b5aee025b54d3f487e21d822050db
Jason Rodriguez <austinwithaustism@Jasons-iMac.local>
2020-07-05 18:08:45 -0400

    edit

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://heroku_81whdc6t:jhGPEHVjjyzpeAlR1@ds033599.mlab.com:33599/heroku_81whdc6t";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.use((req, res, next) => 
 {  
     res.setHeader('Access-Control-Allow-Origin', '*');
